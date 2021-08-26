3fd10b8e30bcdb7f6dea93d2e369c333de1702fd
muratilgun <muratilgun34@gmail.com>
2021-08-26 16:19:52 +0300

    Initial commit

---
+++ RedisDeneme/Controllers/CacheController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RedisDeneme.Contracts;
+using RedisDeneme.Services;
+
+namespace RedisDeneme.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CacheController : ControllerBase
+    {
+        private readonly ICacheService _cacheService;
+
+        public CacheController(ICacheService cacheService)
+        {
+            _cacheService = cacheService;
+        }
+
+        [HttpGet("selam")]
+        public string Selam()
+        {
+            return "selam";
+        }
+
+
+        [HttpGet("cache/{key}")]
+        public async Task<IActionResult> GetCacheValue([FromRoute] string key)
+        {
+            var value = await _cacheService.GetCacheValueAsync(key);
+            return string.IsNullOrEmpty(value) ? (IActionResult)NotFound() : Ok(value);
+
+        }
+
+        [HttpPost("cache")]
+        public async Task<IActionResult> SetCacheValue([FromBody] NewCacheEntryRequest request)
+        {
+            await _cacheService.SetCacheValueAsync(request.Key, request.Value);
+            return Ok();
+        }
+    }
+}
